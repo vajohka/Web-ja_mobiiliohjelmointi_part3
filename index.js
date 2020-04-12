12e2bfb8c000e8a6cd52adc551ef5e05eb2cfd47
vajohka <vajoka@utu.fi>
2020-04-12 21:06:15 +0300

    3.1

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World')
+app.use(express.json())
+
+let henkilot = [
+    {
+        name: "Arto Hellas",
+        number: "040-1286245",
+        id: 1,
+    },
+    {
+        name: "Martti Tienari",
+        number: "040-1286246",
+        id: 2,
+    },
+    {
+        name: "Arto Järvinen",
+        number: "040-1286247",
+        id: 1,
+    },
+    {
+        name: "Lea Kutvonen",
+        number: "040-1286248",
+        id: 1,
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(henkilot)
 })
 
 const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)   
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})   
