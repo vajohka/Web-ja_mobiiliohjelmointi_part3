dcb437fe60cb812f032976d001b89b1e9c041c1b
Valtter <valtterkarppinen@gmail.com>
2020-04-14 19:53:14 +0300

    static

---
+++ build/precache-manifest.0a1749979a10e9269784d59d0dbb0431.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d7d1cf9982dff0435fc",
+    "url": "/static/js/main.2d7d1cf9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "485e79d2609abe007edb",
+    "url": "/static/js/2.485e79d2.chunk.js"
+  },
+  {
+    "revision": "063ed67539f5a4b67f9576705c3fe857",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.970bac8f9adc14ebbc0e3b73244e62bf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "31e0bdcd901733166bc7",
+    "url": "/static/js/main.31e0bdcd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "485e79d2609abe007edb",
+    "url": "/static/js/2.485e79d2.chunk.js"
+  },
+  {
+    "revision": "a6b9e8a50b2c1f7aeece6272a68d3547",
+    "url": "/index.html"
+  }
+];
